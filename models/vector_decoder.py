ea2d44dc109c4b6eb8899ff10d31511fcc774177
Marco Cipriano <marco.cipriano@hpi.de>
2023-08-03 13:43:20 +0200

    supports DDP
    - supports DDP
    - fixes imports

--- models/vector_decoder.py
+++ models/vector_decoder.py
@@ -3,7 +3,7 @@ import random
 import torch
 from torch import Tensor
 import wandb
-from thesis.models import BaseVAE
+from models import BaseVAE
 from torch import nn
 from torch.nn import functional as F
 from PIL import Image
@@ -18,7 +18,7 @@ import torchvision
 import matplotlib.pyplot as plt
 from torchvision import transforms
 
-dsample = kornia.transform.PyrDown()
+dsample = kornia.geometry.transform.pyramid.PyrDown()
 
 
 # import os
