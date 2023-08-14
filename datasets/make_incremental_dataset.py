dbbbd17637471f96959167ae3c50fc66407ff681
Marco Cipriano <marco.cipriano@hpi.de>
2023-08-14 20:02:12 +0200

    adds incremental dataset with policies

--- datasets/make_incremental_dataset.py
+++ datasets/make_incremental_dataset.py
@@ -16,14 +16,22 @@ from matplotlib import pyplot as plt
 import os
 import io
 from tqdm import tqdm
+from pathlib import Path as LinuxPath
 
 
 FIGR8_PATH = "/scratch4/mcipriano/SVG/FIGR-8-SVG/Data"
+OUT_DIR = "/scratch4/mcipriano/SVG/incremental_FIGR-8/"
 OUT_W = 500
 OUT_H = 500
+DEBUG = True
 
 
-def raster(svg_file):
+def raster(svg_file: Drawing):
+    """
+    This function simply resizes and rasters a series of Paths
+    @param svg_file: Drawing object
+    @return: Numpy array of the raster image
+    """
     svg_png_image = svg2png(
         bytestring=svg_file.tostring(),
         output_width=OUT_W,
@@ -33,23 +41,60 @@ def raster(svg_file):
     return img
 
 
-def export_dataset():
-    df = []
-    id_counter = 0
+def export_dataset(policy: str, context_length: int = 50, patience: int = 5):
+    """
+    For each SVG entry of the dataset create a raster versions of each path which is part of that entry.
+    the policy specify how to sort and group each path. Lines which are part of a Path can also be considered
+    as path themselves
+    @param policy: one of: "closed", "length", "position".
+    "closed" returns only closed path (sort by area), "length" and "position" first both break all the continous lines
+    in paths and create a path for each of them. "length" sort them by length, "position" sort them from the top-left
+    corner of the image.
+    @param context_length: ideal number of sub-image for each SVG path
+    @param patience: threshold to discard an SVG image, images with less than this number of paths will be discarded.
+    This applies only if "closed" policy is selected. it does not apply to policies that work with flatten SVG.
+    @return: 0 if export is sucessful
+    """
+
+    assert policy in ["closed", "length", "position"], "Wrong policy or policy not implemented yet!"
+
+    df = pd.DataFrame(columns=['filename', 'class', 'split'])
+
     for folder in os.listdir(FIGR8_PATH):
-        for img_name in os.listdir(os.path.join(FIGR8_PATH, folder)):
+        for image_id, img_name in enumerate(os.listdir(os.path.join(FIGR8_PATH, folder))):
             file_path = os.path.join(FIGR8_PATH, folder, img_name)
             paths, attributes = svg2paths(file_path)
 
-            # Using groups of lines if we have only one path
-            if len(paths) == 1:
-                if len(paths[0]) < 10:
-                    print(f"skip {file_path}, too few lines")
+            if DEBUG:
+                plt.imshow(raster(disvg(paths, paths2Drawing=True)))
+                plt.show()
+
+            # using path as they are results in a loss of very important features
+            # taking all the lines as paths and then grouping connected lines is the best option
+            # also: a closed path must be formed by 1 or more connected lines anyway!
+            paths = [item for sublist in paths for item in sublist]  # flatten paths
+            paths = Path(*paths).continuous_subpaths()  # grouping connected lines
+
+            if policy == "closed":
+                paths = [p for p in paths if p.isclosed()]
+                if len(paths) < patience:
                     continue
-                n_take = round(len(paths[0]) / 5)  # check how many lines to use to have a total of 5 paths
-                paths = [Path(*paths[0][i * n_take: (i + 1) * n_take]) for i in range(5)]  # break lines into paths
-                # paths = [Path(paths[0][i * n_take: (i + 1) * n_take]) for i in range(5)]
-            # TODO: sorting of the paths per importance
+                sort_attrib = [np.abs(p.area()) for p in paths]  # taking paths with larger areas first
+            elif policy == "length":
+                sort_attrib = [Path(p).length() for p in paths]
+            elif policy == "position":
+                sort_attrib = []
+                for p in paths:
+                    bbox = p.bbox()  # returns x_min, x_max, y_min, y_max
+                    sort_attrib.append(np.sqrt(bbox[0] ** 2 + bbox[2] ** 2))
+            else:
+                raise Exception("Wrong policy or policy not implemented yet!")
+
+            # sorting and truncating regardless the policy used
+            # paths = [p for _, p in sorted(zip(sort_attrib, paths), reverse=True)]
+            # using index of enumerate as second sorting params if two occurrences are the same
+            paths = [p for _, p in sorted(enumerate(paths), key=lambda x: (sort_attrib[x[0]], x[0]), reverse=True)]
+            paths = paths[:context_length]  # do not exceed CL
 
             # merging all the paths but hiding each of them
             svg = disvg(paths, paths2Drawing=True)  # merging all the paths
@@ -57,12 +102,28 @@ def export_dataset():
                 svg.elements[i].attribs["visibility"] = "hidden"
 
             # Saving each path -> using visibility attribs in the
+            imgs = []
             for i in range(1, len(paths) + 1):
                 del svg.elements[i].attribs["visibility"]  # showing this path
                 img = raster(svg)
+                imgs.append(img)
                 svg.elements[i].attribs["visibility"] = "hidden"  # restore
-                plt.imshow(img)
-                plt.show()
+                if DEBUG:
+                    plt.imshow(img)
+                    plt.show()
+            np.save(
+                os.path.join(OUT_DIR, policy, f"{folder[0]}_{image_id}.npy"),
+                np.stack(imgs)
+            )
+            new_row = {
+                "filename": f"{folder[0]}_{image_id}.npy",
+                "class": folder,
+                "split": "train"
+            }
+            df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+
+    # save final csv
+    df.to_csv(os.path.join(OUT_DIR, policy, 'split.csv'), index=False)
 
 
 def compute_stats():
@@ -89,7 +150,9 @@ def compute_stats():
 
 
 if __name__ == '__main__':
-    export_dataset()
+    policy = "position"  # -> "closed", "length", "position"
+    LinuxPath(os.path.join(OUT_DIR, policy)).mkdir(parents=True, exist_ok=True)
+    export_dataset(policy)
 
 
 
