4ecc4a7ecdeadad5e15e2423756d616a1d5b9cdc
Marco Cipriano <marco.cipriano@hpi.de>
2023-08-17 11:19:14 +0200

    fixes shift between input and gt

--- dataset.py
+++ dataset.py
@@ -211,6 +211,56 @@ class NounProject(Dataset):
         return len(self.image_paths)
 
 
+class Figr8CausalSVGDataset(Dataset):
+    """
+    FIGR8 dataset from a root directory for causal svg generation
+
+     Lenght
+     |
+     |---------------------------------------------|
+     F{folder_id}_I{svg_id}_P{features}.npy    split.csv
+    """
+
+    def __init__(self, root_path: str, context_length: int, channels: int, width: int, **kwargs):
+        super(Figr8CausalSVGDataset, self)
+        self.context_length = context_length
+        self.channels = channels
+        self.width = width  # TODO: can we remove this?
+        self.root_path = root_path
+        self.split = pd.read_csv(os.path.join(self.root_path, "split.csv"))
+        self.split = self.split[self.split["split"] == ("train" if kwargs["train"] else "test")]
+
+    def __getitem__(self, index) -> tuple:
+        filename = self.split.iloc[index]["filename"]
+        images = np.load(os.path.join(self.root_path, filename))
+        images = torch.from_numpy(images)
+        num_features = images.shape[0]
+        if images.max() > 1:
+            images = images / 255  # shift to [0-1] (imgs stored as uint8)
+
+        # padding images with fewer features than CL
+        pad_len = self.context_length - num_features
+        pad = torch.ones(pad_len, *images.shape[1:])  # 1 -> white -> no features
+        shape_layers = torch.concat((images, pad), dim=0)  # Ground truth
+
+        # adding channel (Dataset is gray-scale, if you use 3 channels those are replicated)
+        shape_layers = shape_layers[:, None].repeat((1, self.channels, 1, 1))
+
+        # input is all shifted one place to the right and starts with white canvas
+        images = torch.concat((torch.ones(1, self.channels, *images.shape[1:]), shape_layers[:-1]))
+
+        # creating binary stop ground truth
+        stop_signals = torch.zeros(self.context_length)
+        stop_signals[num_features:] = 1.
+
+        # TODO: conditioning on captions, class for the image can be found in  self.split.iloc[index]["class"]
+
+        return images, shape_layers, stop_signals
+
+    def __len__(self):
+        return len(self.split)
+
+
 class MNISTDataset(LightningDataModule):
     """
     PyTorch Lightning data module
@@ -496,52 +546,6 @@ class EmojiDataset(LightningDataModule):
             shuffle=True,
             pin_memory=self.pin_memory,
         )
-
-
-class Figr8CausalSVGDataset(Dataset):
-    """
-    returns black dummy images as the full composite images that act as input
-    returns individual shape renderings for loss calculation. full image at index T requires the network to predict shape rendering at index T.
-    returns stop signal vector
-    """
-
-    def __init__(self, root_path: str, context_length: int, channels: int, width: int, **kwargs):
-        super(Figr8CausalSVGDataset, self)
-        self.context_length = context_length
-        self.channels = channels
-        self.width = width
-        self.root_path = root_path
-        self.split = pd.read_csv(os.path.join(self.root_path, "split.csv"))
-        self.split = self.split[self.split["split"] == ("train" if kwargs["train"] else "test")]
-
-    def __getitem__(self, index) -> tuple:
-        filename = self.split.iloc[index]["filename"]
-        images = np.load(os.path.join(self.root_path, filename))
-        images = torch.from_numpy(images)
-        num_features = images.shape[0]
-        if images.max() > 1:
-            images = images / 255  # shift to [0-1] (imgs stored as uint8)
-
-        # padding images with fewer features than CL
-        pad_len = self.context_length - num_features
-        pad = torch.ones(pad_len, *images.shape[1:])  # 1 -> white -> no features
-        images = torch.concat((images, pad), dim=0)
-
-        # adding channel (Dataset is gray-scale, if you use 3 channels those are replicated)
-        images = images[:, None].repeat((1, self.channels, 1, 1))
-
-        # creating binary stop ground truth
-        stop_signals = torch.zeros(self.context_length)
-        stop_signals[num_features:] = 1.
-
-        shape_layers = images  # TODO: kept this for backward-compatibility, do we need it?
-
-        # TODO: conditioning on captions, class for the image can be found in  self.split.iloc[index]["class"]
-
-        return images, shape_layers, stop_signals
-
-    def __len__(self):
-        return len(self.split)
 
 
 class CausalSVGDataModule(LightningDataModule):
