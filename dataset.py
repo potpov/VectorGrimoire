fixes causal mnist dataset

dataset.py
@@ -329,12 +329,12 @@ class MNISTforCSVG(Dataset):
             image = self.transform(image)
         
         width = image.size(1)
-        black_image = torch.ones((3, width, width)) # needs to be black because of MNIST inversion transformation
-        images = torch.stack([black_image] + [image]*(self.context_length-1), dim=0)
-        shape_layers = torch.stack([image]*(self.context_length), dim=0)
+        white_image = torch.ones((3, width, width))
+        input_images = torch.stack([white_image] + [image] + [white_image]*(max(self.context_length-2, 0)), dim=0)
+        gt_shape_layers = torch.stack([image] + [white_image]*(self.context_length-1), dim=0)
         stop_signals = torch.cat([torch.Tensor([0.]), torch.Tensor([1.]*(self.context_length-1))], dim=0)
 
-        return images, shape_layers, stop_signals
+        return input_images, gt_shape_layers, stop_signals#, label
 
     def __len__(self):
         return len(self.image_paths)