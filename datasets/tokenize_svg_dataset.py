c67bdff522a3e7b4725237f8f61c477fe9ca3a0e
Moritz Feuerpfeil <moritz.feuerpfeil@student.hpi.uni-potsdam.de>
2024-02-09 22:49:46 +0100

    Update tokenization script to use new dataset and configuration

--- datasets/tokenize_svg_dataset.py
+++ datasets/tokenize_svg_dataset.py
@@ -1,55 +1,85 @@
 from thesis.tokenizer import VQTokenizer
-from thesis.dataset import CenterShapeLayersFromSVGDataset
+from thesis.dataset import GlyphazznStage1Dataset
 from thesis.models import Vector_VQVAE
 import numpy as np
 import torch
 from glob import glob
 import os
 from tqdm import tqdm
+import yaml
+import pandas as pd
 
 def main():
     CONTEXT_LENGTH = 256
-    MODEL_WEIGHTS_PATH = "/scratch2/moritz_logs/SVG_VQVAE/Stage1/glyphazzn_B_simplified_single_code/checkpoints/last-v3.ckpt"
-    OUT_DIR = "/scratch2/moritz_data/glyphazzn/B_simplified/tokenized"
+    MODEL_WEIGHTS_PATH = "/scratch2/moritz_logs/SVG_VQVAE/Stage1/glyphazzn_full_single_code/checkpoints/last.ckpt"
+    OUT_DIR = "/scratch2/moritz_data/glyphazzn/tokenized"
+    TOP_LEVEL_DIR = "/scratch2/moritz_data/glyphazzn/svgs_simplified"
+    CONFIG_PATH = "/home/mfeuerpfeil/master/thesis/configs/SVG_VQVAE_Stage1.yaml"
+
+    with open(CONFIG_PATH, 'r') as file:
+        try:
+            config = yaml.safe_load(file)
+        except yaml.YAMLError as exc:
+            print(exc)
+    config['data_params']["max_shapes_per_svg"] = CONTEXT_LENGTH//2  # because each shape also needs a position token
+        
+    csv_path = os.path.join(TOP_LEVEL_DIR, "split.csv")
+    df = pd.read_csv(csv_path)
     if not os.path.exists(OUT_DIR):
         os.makedirs(OUT_DIR)
-    # load path to all svg files
-    csv_path = "/scratch2/moritz_data/glyphazzn/B_simplified/split.csv"
-    print("Loading dataset..")
-    ds_train = CenterShapeLayersFromSVGDataset(csv_path, 3, 128, train=True, individual_max_length=7.5, stroke_width=0.7, max_shapes_per_svg=CONTEXT_LENGTH//2)
-    ds_test = CenterShapeLayersFromSVGDataset(csv_path, 3, 128, train=False, individual_max_length=7.5, stroke_width=0.7, max_shapes_per_svg=CONTEXT_LENGTH//2)
+    
+    
+    train_df = df[df["split"] == "train"]
+    test_df = df[df["split"] == "test"]
+    train_paths = train_df["path"].values
+    test_paths = test_df["path"].values
+
     print("Loading model..")
-    model = Vector_VQVAE(vq_method="fsq")
+    model = Vector_VQVAE(**config['model_params'])
     state_dict = torch.load(MODEL_WEIGHTS_PATH)["state_dict"]
     try:
         model.load_state_dict(state_dict)
     except:
         model.load_state_dict({k.replace("model.", ""): v for k, v in state_dict.items()})
     model = model.eval()
-    tokenizer = VQTokenizer(model, 128, CONTEXT_LENGTH, 1)
+    tokenizer = VQTokenizer(model, config["data_params"]["width"], 1, "bert-base-uncased")
+
+    print("Loading dataset..")
+    ds_train = GlyphazznStage1Dataset(train=True, **config['data_params'])
+    ds_test = GlyphazznStage1Dataset(train=False, **config['data_params'])
     print("Number of Tokens: ",tokenizer.num_tokens)
 
-    train_tokens = []
+    train_vq_tokens = []
+    train_text_tokens = []
     print("Processing training set..")
     for i in tqdm(range(len(ds_train))):
-        imgs, _, centers = ds_train[i]
-        tokens = tokenizer.tokenize(imgs, centers, return_np_uint16=True)
-        train_tokens.append(tokens)
+        imgs, label, centers = ds_train._get_full_item(i)
+        start_token, text_tokens, vq_tokens, end_token = tokenizer.tokenize(imgs, centers, text=label, return_np_uint16=True)
+        train_vq_tokens.append(vq_tokens)
+        train_text_tokens.append(text_tokens)
         if i % 100 == 0:
-            np.save(os.path.join(OUT_DIR, "train_tokens.npy"), np.concatenate(train_tokens))
-    train_tokens = np.concatenate(train_tokens)
-    np.save(os.path.join(OUT_DIR, "train_tokens.npy"), train_tokens)
+            np.save(os.path.join(OUT_DIR, "train_vq_tokens.npy"), np.concatenate(train_vq_tokens))
+            np.save(os.path.join(OUT_DIR, "train_text_tokens.npy"), np.concatenate(train_text_tokens))
+    train_vq_tokens = np.concatenate(train_vq_tokens)
+    train_text_tokens = np.concatenate(train_text_tokens)
+    np.save(os.path.join(OUT_DIR, "train_vq_tokens.npy"), train_vq_tokens)
+    np.save(os.path.join(OUT_DIR, "train_text_tokens.npy"), train_text_tokens)
 
-    test_tokens = []
+    test_vq_tokens = []
+    test_text_tokens = []
     print("Processing test set..")
     for i in tqdm(range(len(ds_test))):
-        imgs, _, centers = ds_test[i]
-        tokens = tokenizer.tokenize(imgs, centers, return_np_uint16=True)
-        test_tokens.append(tokens)
+        imgs, label, centers = ds_test._get_full_item(i)
+        start_token, text_tokens, vq_tokens, end_token = tokenizer.tokenize(imgs, centers, text=label, return_np_uint16=True)
+        test_vq_tokens.append(vq_tokens)
+        test_text_tokens.append(text_tokens)
         if i % 100 == 0:
-            np.save(os.path.join(OUT_DIR, "test_tokens.npy"), np.concatenate(test_tokens))
-    test_tokens = np.concatenate(test_tokens)
-    np.save(os.path.join(OUT_DIR, "test_tokens.npy"), test_tokens)
+            np.save(os.path.join(OUT_DIR, "test_vq_tokens.npy"), np.concatenate(test_vq_tokens))
+            np.save(os.path.join(OUT_DIR, "test_text_tokens.npy"), np.concatenate(test_text_tokens))
+    test_vq_tokens = np.concatenate(test_vq_tokens)
+    test_text_tokens = np.concatenate(test_text_tokens)
+    np.save(os.path.join(OUT_DIR, "test_vq_tokens.npy"), test_vq_tokens)
+    np.save(os.path.join(OUT_DIR, "test_text_tokens.npy"), test_text_tokens)
 
 if __name__ == '__main__':
     main()
