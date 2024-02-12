41c3b2430129d64e1f30b41b2b64b6786a2d68e3
Moritz Feuerpfeil <moritz.feuerpfeil@student.hpi.uni-potsdam.de>
2024-02-12 23:21:13 +0100

    Add VQ_SVG_Stage2 model and dependencies

--- models/__init__.py
+++ models/__init__.py
@@ -2,12 +2,13 @@ from .base import *
 from .vanilla_vae import *
 from .vq_vae import *
 from .vector_vae_nlayers import *
-from models.vaectorgen import VAEctorGen
+from .vaectorgen import VAEctorGen
 from .vectorGPT import VectorGPT
 from .vectorGPTv2 import VectorGPTv2
-from .svg_vqvae import Vector_VQVAE, VQ_Transformer
+from .svg_vqvae import Vector_VQVAE, VQ_Transformer_deprecated
+from .stage2 import VQ_SVG_Stage2
 
-vae_models = {"VanillaVAE": VanillaVAE, 
+vae_models = {"VanillaVAE": VanillaVAE,
               "VQVAE": VQVAE,
               "Im2Vec" : VectorVAEnLayers,
               "VAEctorGen" : VAEctorGen}
