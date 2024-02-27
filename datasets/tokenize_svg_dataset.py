c5220b6652ce1af3e00b7a07912356ba3c5d53be
Marco Cipriano <marco.cipriano@hpi.de>
2024-02-27 11:37:33 +0100

    uses one npy files for tokenization

--- datasets/tokenize_svg_dataset.py
+++ datasets/tokenize_svg_dataset.py
@@ -9,7 +9,7 @@ import yaml
 import resource
 
 from torch.utils.data import DataLoader
-
+import pandas as pd
 import re
 from dataset import GlyphazznStage1Dataset
 import torch.nn as nn
@@ -51,9 +51,11 @@ def main():
     rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
     resource.setrlimit(resource.RLIMIT_NOFILE, (2048, rlimit[1]))
 
-    MODEL_WEIGHTS_PATH = "/scratch/mcipriano/results/svg/all_full_single_code/checkpoints/last-v1.ckpt"
+    # MODEL_WEIGHTS_PATH = "/scratch/mcipriano/results/svg/all_full_single_code/checkpoints/last-v1.ckpt"
+    # MODEL_WEIGHTS_PATH = "/scratch/mcipriano/results/svg/figr8/checkpoints/epoch=0-step=10500.ckpt"
+    MODEL_WEIGHTS_PATH = "/scratch/mcipriano/cache/svg/moritz_geometric.ckpt"
     CONFIG_PATH = "/home/mcipriano/projects/SVG/Moritz/configs/SVG_VQVAE_Stage1.yaml"
-    BATCH_SIZE = 64
+    BATCH_SIZE = 16
 
     with open("font_paths.yaml", "r") as stream:
         font_config = yaml.safe_load(stream)
@@ -69,8 +71,9 @@ def main():
 
         config['data_params']["max_shapes_per_svg"] = 512  # more than context length of 1024 (at least one patch and one pos token per svg shape) we'll never do I think
 
-        Path(os.path.join(params["tokenized"], "train")).mkdir(parents=True, exist_ok=True)
-        Path(os.path.join(params["tokenized"], "test")).mkdir(parents=True, exist_ok=True)
+        Path(os.path.join(params["tokenized"])).mkdir(parents=True, exist_ok=True)
+        # Path(os.path.join(params["tokenized"], "train")).mkdir(parents=True, exist_ok=True)
+        # Path(os.path.join(params["tokenized"], "test")).mkdir(parents=True, exist_ok=True)
         # if len(os.listdir(os.path.join(params["tokenized"], "train"))) > 0:
         #     print("Output directory is not empty, found: ", os.listdir(params["tokenized"]))
         #     input("Press Enter to continue...")
@@ -91,9 +94,11 @@ def main():
         tokenizer = VQTokenizer(model, config["data_params"]["width"], 1, "bert-base-uncased")
 
         print("Loading dataset..")
-        existing_data = get_existing_Data(params["tokenized"])
+        # existing_data = get_existing_Data(params["tokenized"])
+        existing_data = []
 
         config['data_params']["top_level_dir"] = params["svg_simp"]
+        print("loading from: ", config['data_params']["top_level_dir"])
         ds_train = SkipDataset(
             GlyphazznStage1Dataset(train=True, **config['data_params'], return_filename=True),
             existing_data
@@ -122,16 +127,25 @@ def main():
         print("Number of Tokens: ", tokenizer.num_tokens)
 
         print("Processing training set..")
+
+        save_csv = {"index_in_numpy_array": [], "class": [], "split": []}
+        numpy_arrays = []
+        numpy_counter = 0
         for split_name, split in {"train": dl_train, "test": dl_test}.items():
             for i, batch in tqdm(enumerate(split), total=len(split), desc=f"processing {split_name}"):
                 imgs, labels, centers, descriptions, filenames = batch
                 for img, label, center, description, filename in zip(imgs, labels, centers, descriptions, filenames):
                     start_token, text_tokens, vq_tokens, end_token = tokenizer.tokenize(img.to(device), center, text=description, return_np_uint16=True)
                     class_id, name = filename.split("/")[-2:]
-                    Path(os.path.join(params["tokenized"], split_name, class_id)).mkdir(parents=True, exist_ok=True)
-                    np.save(os.path.join(params["tokenized"], split_name, class_id, "VQ_" + name.replace("svg", "npy")), vq_tokens)
-                    np.save(os.path.join(params["tokenized"], split_name, class_id, "TXT_" + name.replace("svg", "npy")), text_tokens)
+                    save_csv["index_in_numpy_array"].append(numpy_counter)
+                    save_csv["class"].append(class_id)
+                    save_csv["split"].append(split_name)
+                    numpy_arrays.append(vq_tokens)
+                    numpy_counter += 1
 
+        np.save(os.path.join(params["tokenized"], "tokenized.npy"), np.concatenate(numpy_arrays))
+        df = pd.DataFrame(save_csv)
+        df.to_csv(os.path.join(params["tokenized"], "split.csv"), index=False)
 
 if __name__ == '__main__':
     # print(get_latest_data_checkpoint("/scratch2/moritz_data/glyphazzn"))
